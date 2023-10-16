c53544a700dd3cd06c006198ab651fb0ec60df72
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-10-16 17:42:21 +0300

    default-page-logged-user-middleware

---
+++ cartridges/custom_cartridge_1/cartridge/controllers/Default.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * @namespace Default
+ */
+
+var server = require("server");
+server.extend(module.superModule)
+var userLoggedIn = require("*/cartridge/scripts/middleware/userLoggedIn");
+
+/** when sitepath is defined in the site aliases from business manager, homepage will be rendered directly */
+/**
+ * Default-Start : This end point is the root of the site, when opening from the BM this is the end point executed
+ * @name Base/Default-Start
+ */
+server.get(
+    "Start",
+    userLoggedIn.validateLoggedIn,
+);
+
+module.exports = server.exports();
