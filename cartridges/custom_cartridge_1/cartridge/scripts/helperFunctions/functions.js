48b8108317508553875f734ee03782df8ce56a91
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-10-18 18:24:04 +0300

    correct functon that gets get catalog main categories

--- cartridges/custom_cartridge_1/cartridge/scripts/helperFunctions/functions.js
+++ cartridges/custom_cartridge_1/cartridge/scripts/helperFunctions/functions.js
@@ -1,60 +1,57 @@
-// Create a function to get product by a given ID
-// Create a function to get product category by given product
-// Create a function to get different product prices for a given product
-// Create a function to get catalog main categories
-// Create a function to get customer by ID
-// Create a function to check if a given customer is assigned to a given customer group
-
-// Create a function to get product by a given ID
 /**
+ * Create a function to get product by a given ID
  * @param {dw.catalog.Product} product
  * @returns {String}
  */
 function getProductByGivenID(id) {
-    product.getID();
+    return product.getID();
 }
 
-// Create a function to get product category by given product
 /**
+ * Create a function to get product category by given product
  * @param {dw.catalog.Product} product
  * @returns {Collection}
  */
 function getProductCategoryByGivenProduct(product) {
-    product.getCategories();
+    return product.getCategories();
+
 }
 
-// Create a function to get different product prices for a given product
 /**
+ * Create a function to get different product prices for a given product
  * @param {dw.catalog.Product} product
  * @returns {ProductPriceModel}
  */
 function getDifferentProductPricesForGivenProduct(product) {
-    product.getPriceModel();
+    return product.getPriceModel();
 }
 
-// Create a function to get catalog main categories
 /**
+ *  Create a function to get catalog main categories
  * @param {dw.catalog.Catalog} catalog
- * @returns {Object}
+ * @returns {Collection}
  */
-function getCatalogMainCategories(catalog) {
-    catalog.getRoot();
+function getCatalogMainCategories() {
+    // Returns the catalog of the current site or null if no catalog is assigned to the site.
+    var siteCatalog = CatalogMgr.getSiteCatalog();
+   
+    return siteCatalog.getRoot().getSubCategories();;
 }
 
-// Create a function to get customer by ID
 /**
+ * Create a function to get customer by ID
  * @param {dw.customer.Customer} customer 
  * @returns {String}
  */
 function getCustomerByID(customer) {
-    customer.getID();
+    return customer.getID();
 }
 
-// Create a function to check if a given customer is assigned to a given customer group
 /**
+ *  Create a function to check if a given customer is assigned to a given customer group
  * @param {dw.customer.Customer} customer
  * @returns {Boolean} 
  */
 function checkIfCustomerIsAssignedToGivenCustomerGroup(customer, groupID) {
-    customer.isMemberOfCustomerGroup(groupID);
+    return customer.isMemberOfCustomerGroup(groupID);
 }
