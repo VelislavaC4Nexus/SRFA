d88b5f74297d36823d05e0033254095bb8198e52
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-10-19 09:22:12 +0300

    content slot type html

--- cartridges/custom_cartridge_1/cartridge/controllers/SlotDemo.js
+++ cartridges/custom_cartridge_1/cartridge/controllers/SlotDemo.js
@@ -1,21 +1,21 @@
 "use strict";
 
 /**
- * @namespace DWScriptDemo
+ * @namespace SlotDemoDemo
  */
 
 var server = require("server");
 var cache = require("*/cartridge/scripts/middleware/cache");
 var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
 var pageMetaData = require("*/cartridge/scripts/middleware/pageMetaData");
-const DwScriptModel = require("*/cartridge/models/dwScriptModel");
+
 
 /**
  * Any customization on this endpoint, also requiresss update for Default-Start endpoint
  */
 /**
  * @function
- * @memberof DwScriptDemo
+ * @memberof SlotDemo
  * @param {middleware} - consentTracking.consent
  * @param {middleware} - cache.applyDefaultCache
  * @param {category} - non-sensitive
@@ -29,12 +29,11 @@ server.get(
     function (req, res, next) {
         var Site = require("dw/system/Site");
         var pageMetaHelper = require("*/cartridge/scripts/helpers/pageMetaHelper");
-        let dwScriptModel = {name: "AAA", id:"123",age:"23"}
 
         pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
 
-        res.render("dwScriptDemo", dwScriptModel);
-        
+        res.render("slotTemplate");
+       
         next();
     },
     pageMetaData.computedPageMetaData
