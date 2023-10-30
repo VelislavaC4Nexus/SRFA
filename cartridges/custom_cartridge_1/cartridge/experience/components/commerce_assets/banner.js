6e92fc4bbff191b7f8ffda5189bf95fcf6599924
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-10-30 11:16:10 +0200

    fix indentation

--- cartridges/custom_cartridge_1/cartridge/experience/components/commerce_assets/banner.js
+++ cartridges/custom_cartridge_1/cartridge/experience/components/commerce_assets/banner.js
@@ -5,7 +5,7 @@ var HashMap = require('dw/util/HashMap');
  * Render logic for the component.
  */
 module.exports.render = function (context) {
-   var model = new HashMap();
-   model.bannerMessage = context.content.alt;
- return new Template('experience/components/commerce_assets/banner').render(model).text;
+  var model = new HashMap();
+  model.bannerMessage = context.content.alt;
+  return new Template('experience/components/commerce_assets/banner').render(model).text;
 };
