e262ab392e617e351c1474763f1d83e44df22002
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-11-03 10:51:08 +0200

    custom object job write CSV

---
+++ cartridges/int_newsletter/cartridge/scripts/steps/NewsletterJob.js
@@ -0,0 +1,39 @@
+
+var CustomObjectMgr = require("dw/object/CustomObjectMgr");
+var Transaction = require("dw/system/Transaction");
+var { File, FileWriter, CSVStreamWriter } = require("dw/io");
+var Logger = require("dw/system/Logger");
+
+module.exports.execute = function () {
+    var newsletterObjectIterator = CustomObjectMgr.getAllCustomObjects('NEWSLETTER_SUBSCRIPTION_CO_HW');
+    var csvSw;
+    var file;
+    var fileWriter;
+
+    try {
+        file = new File([File.IMPEX, "objects.csv"].join(File.SEPARATOR));
+        fileWriter = new FileWriter(file);
+
+        csvSw = new CSVStreamWriter(fileWriter);
+
+        var customAttributes = ["firstname", "lastname", "email", "gender"];
+        csvSw.writeNext(customAttributes)
+     
+        while (newsletterObjectIterator.hasNext()) {
+            var newsletterPresentation = newsletterObjectIterator.next();
+
+            csvSw.writeNext([
+                newsletterPresentation.custom.firstname,
+                newsletterPresentation.custom.lastname,
+                newsletterPresentation.custom.email,
+                newsletterPresentation.custom.gender,
+            ]);
+        }
+    } catch (e) {
+        var logger = Logger.getLogger('DemoLog', 'Job');
+        logger.error(e.message)
+    } finally {
+        csvSw.close();
+        fileWriter.close();
+    }
+}
